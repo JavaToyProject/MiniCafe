75a93766cd7dc79571dbcd6961e76585ddbdf457
Rohdamin <rohdamin3@gmail.com>
2024-07-28 14:29:37 +0900

    Fix: Beverage file 경로 변경

--- src/main/java/minicafe/beverage/repository/BeverageRepository.java
+++ src/main/java/minicafe/beverage/repository/BeverageRepository.java
@@ -18,7 +18,7 @@ public class BeverageRepository {
      *  3. beverageArrayList에 음료 정보 저장
      * */
     public BeverageRepository() {
-        File file = new File("src/main/java/beverage/db/beverageDB.dat");
+        File file = new File("src/main/java/minicafe/beverage/db/beverageDB.dat");
 
         if (!file.exists()) {
             ArrayList<Beverage> defaultBeverage = new ArrayList<>();
@@ -115,7 +115,7 @@ public class BeverageRepository {
         /* 개인필기. 헤더 없이 파일에 추가하고, 수정된 파일로 beverageArrayList 업데이트해야 함 */
 
         int result = 0;
-        File file = new File("src/main/java/beverage/db/beverageDB.dat");
+        File file = new File("src/main/java/minicafe/beverage/db/beverageDB.dat");
 
         MyObjectOutput moo = null;
 
@@ -152,7 +152,7 @@ public class BeverageRepository {
                 if (reformBeverage.getStock() != 0) beverage.setStock(reformBeverage.getStock());
 
                 // 수정된 beverageArrayList 전체를 파일에 덮어씌우기
-                File file = new File("src/main/java/beverage/db/beverageDB.dat");
+                File file = new File("src/main/java/minicafe/beverage/db/beverageDB.dat");
                 saveBeverages(file, beverageArrayList);
 
                 return 1;
@@ -167,7 +167,7 @@ public class BeverageRepository {
                 beverageArrayList.remove(i);
 
                 // 삭제된 beverageArrayList 전체를 파일에 덮어씌우기
-                File file = new File("src/main/java/beverage/db/beverageDB.dat");
+                File file = new File("src/main/java/minicafe/beverage/db/beverageDB.dat");
                 saveBeverages(file, beverageArrayList);
 
                 return 1;
